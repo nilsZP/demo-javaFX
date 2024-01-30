8a776b8af5f4df540963ea7fae2b7ea2652f47dd
nilsZP <nils.vandevelde@bazandpoort.be>
2024-01-30 12:01:19 +0100

    update

--- src/main/java/com/example/demo/PersonenScherm.java
+++ src/main/java/com/example/demo/PersonenScherm.java
@@ -4,11 +4,10 @@ import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -41,14 +40,14 @@ public class PersonenScherm {
                 Stage detailStage = maakDetailStage(tableView.getSelectionModel().getSelectedItem());
                 detailStage.show();
             }
-        });
+        });*/
         toevoegenKnop.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
                 Stage toevoegenStage = maakToevoegenStage(lijst);
                 toevoegenStage.show();
             }
-        });*/
+        });
         verwijderenKnop.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
@@ -77,5 +76,78 @@ public class PersonenScherm {
         personenStage.setTitle("Vereniging " + vereniging + ": leden");
         personenStage.setScene(new Scene(personenContainer,550,550));
         return personenStage;
+    }
+
+    public static Stage maakToevoegenStage(ObservableList<Persoon> lijst) {
+        Stage persoonToevoegenStage = new Stage();
+        GridPane persoonToevoegenContainer = new GridPane();
+
+        persoonToevoegenContainer.setHgap(5);
+        persoonToevoegenContainer.setVgap(5);
+        persoonToevoegenContainer.setPadding(new Insets(10));
+
+        Label gebruikersnaamLabel = new Label("Gebruikersnaam: ");
+        persoonToevoegenContainer.add(gebruikersnaamLabel,0,0);
+        TextField gebruikersnaamVeld = new TextField();
+        persoonToevoegenContainer.add(gebruikersnaamVeld,1,0);
+
+        Label voornaamLabel = new Label("Voornaam: ");
+        persoonToevoegenContainer.add(voornaamLabel,0,1);
+        TextField voornaamVeld = new TextField();
+        persoonToevoegenContainer.add(voornaamVeld,1,1);
+
+        Label familienaamLabel = new Label("Familienaam: ");
+        persoonToevoegenContainer.add(familienaamLabel,0,2);
+        TextField familienaamVeld = new TextField();
+        persoonToevoegenContainer.add(familienaamVeld,1,2);
+
+        Label emailLabel = new Label("Email: ");
+        persoonToevoegenContainer.add(emailLabel,0,3);
+        TextField emailVeld = new TextField();
+        persoonToevoegenContainer.add(emailVeld,1,3);
+
+        Label aantalkinderenLabel = new Label("Aantal kinderen: ");
+        persoonToevoegenContainer.add(aantalkinderenLabel,0,4);
+        Spinner<Integer> aantalKinderenVeld = new Spinner<>(0,50,0);
+        persoonToevoegenContainer.add(aantalKinderenVeld,1,4);
+
+        Button toevoegenKnop = new Button("Toevoegen");
+        persoonToevoegenContainer.add(toevoegenKnop,0,5);
+        toevoegenKnop.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                lijst.add(new Persoon(gebruikersnaamVeld.getText(),voornaamVeld.getText(),familienaamVeld.getText(),
+                        emailVeld.getText(),aantalKinderenVeld.getValue()));
+                persoonToevoegenStage.close();
+            }
+        });
+
+        persoonToevoegenStage.setTitle("Lid toevoegen");
+        persoonToevoegenStage.setScene(new Scene(persoonToevoegenContainer,800,400));
+
+        return persoonToevoegenStage;
+    }
+
+    public static Stage maakDetailScherm(Persoon persoon) {
+        Stage detailStage = new Stage();
+        VBox detailContainer = new VBox(5);
+        detailContainer.setPadding(new Insets(10));
+
+        Label gebruikersnaamLabel = new Label("Gebruikersnaam: " + persoon.getGebruikersnaam());
+        Label voornaamLabel = new Label("Voornaam: " + persoon.getVoornaam());
+        Label familienaamLabel = new Label("Familienaam: " + persoon.getFamilienaam());
+        Label emailLabel = new Label("Email: " + persoon.getEmail());
+        Label aantalkinderenLabel = new Label("Aantal kinderen: " + persoon.getAantalKinderen());
+
+        Button sluitKnop = new Button("Sluiten");
+        sluitKnop.setOnMouseClicked(mouseEvent -> {
+            detailStage.close();
+        });
+
+        detailContainer.getChildren().addAll(gebruikersnaamLabel,voornaamLabel,familienaamLabel,emailLabel,aantalkinderenLabel,sluitKnop);
+        detailStage.setTitle("Details van " + persoon.getGebruikersnaam() + ":");
+        detailStage.setScene(new Scene(detailContainer,800,400));
+
+        return detailStage;
     }
 }
